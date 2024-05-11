58bda9eef471d06e3aba3a06c89c1772d7a87ce8
2pk1Mitrofanov <145149946+2pk1Mitrofanov@users.noreply.github.com>
2024-05-11 16:25:50 +0500

    Add files via upload

---
+++ DOP-main/DOP-main/demo/src/main/java/com/simpeRestApi/demo/controllers/HomeController.java
@@ -0,0 +1,30 @@
+package com.simpeRestApi.demo.controllers;
+
+import com.simpeRestApi.demo.models.TemplateModel;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import javax.xml.crypto.Data;
+import java.sql.Timestamp;
+import java.util.Date;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+@Controller
+@RequestMapping("/rest/home")
+public class HomeController {
+
+    @ResponseBody
+    @RequestMapping(value = "", method=RequestMethod.GET)
+    public ResponseEntity<TemplateModel> response(){
+
+        Date date = new Date();
+        Timestamp timestamp = new Timestamp(date.getTime());
+
+        TemplateModel model = new TemplateModel();
+        model.setMessage("json-ответ");
+        model.setTime((timestamp.toString()));
+
+        return ResponseEntity.ok(model);
+    }
+}
